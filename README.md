f1cdb3af9b80eec5ff0c09d84aef55a51f1653f6
Arijit Mazumdar <arijit.mazumdar@gmail.com>
2022-10-08 23:59:22 +0530

    First version with forward setting..

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# kafka-schema-evolution
+# Kafka Schema Evolution Compatibility
+This project is to test Kafka schema evolution compatibility. This project has been built along with Confluent platform local. But hopefully will work with the other Kafka environments as well.
+
+## Forward
+
+## Backward
---
+++ src/test/java/com/arijit/ConsumerTest.java
@@ -0,0 +1,20 @@
+package com.arijit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ConsumerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
